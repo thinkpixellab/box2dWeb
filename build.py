304309575a0f5f34142eaa9ec9f64d76b52c487c
Kevin Moore <kevin@thinkpixellab.com>
2010-10-29 09:51:08 -0700

    Cleaned up build.py

--- build.py
+++ build.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
-import os
+from os import path
 from _tools.Closure import Closure
-from _tools import HtmlPost
 from glob import glob
-from _tools.HtmlCompressor import HtmlCompressor
 
 js_path = "js"
 
-closure_path = os.path.join(js_path, 'closure-library','closure')
+closure_path = path.join(js_path, 'closure-library','closure')
 
-js_dirs = map(lambda dir: os.path.join(js_path, dir), ['box2d','demo','pixelLab'])
-application_js_path = os.path.join(js_path, 'application.js')
-deps_js_path = os.path.join(js_path, "deps.js")
-compiled_js_path = os.path.join(js_path, "compiled.js")
+js_dirs = map(lambda dir: path.join(js_path, dir), ['box2d','demo','pixelLab'])
+application_js_path = path.join(js_path, 'application.js')
+deps_js_path = path.join(js_path, "deps.js")
+compiled_js_path = path.join(js_path, "compiled.js")
 
-externs = glob(os.path.join(js_path, 'externs', '*.js'))
+externs = glob(path.join(js_path, 'externs', '*.js'))
 
 
 Closure(
